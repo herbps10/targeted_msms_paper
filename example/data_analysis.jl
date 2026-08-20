75a4fd92d0d17d911845b678164444a97c03ef0d
Herb Susmann <herbps10@gmail.com>
2026-08-20 16:22:39 -0400

    updated analysis

--- example/data_analysis.jl
+++ example/data_analysis.jl
@@ -1,87 +1,35 @@
-using TargetedMSM
-using LinearAlgebra
-using Distributions
-using Random
-using DataFrames
-using DataFramesMeta
-using ProgressMeter
-using Optim
-using CSV
-using Plots
-using StatsPlots
-using StatsFuns
-using GLM
 
-data = DataFrame(CSV.File("./pnas_data.csv"))
+\subsection{Targeted Minimum Loss-based Estimation}
+The motivation behind targeted estimators can be seen by analyzing the properties of a plug-in estimator. Suppose we have an initial estimate $\hat{P}$ of the data-generating distribution $\P$. The von Mises expansion reveals that the error of a plug-in estimator $\psi(\P)$ based on $\P$ decomposes as
+\begin{align}
+    \psi(\hat{P}) - \psi(\P) = \E_{P}\left\{ \D_{\hat{P}}(Z) \right\} + \R(\P, \hat{P}). 
+\end{align}
+The first term on the right hand side represents the first-order bias of the plug-in estimator. The second term is the second-order bias of the plug-in estimator. To show consistency of the plug-in estimator, we would need to show that both terms converge to zero asymptotically, which is difficult to ensure in a general setting. 
 
-function linear_working_model(beta::Vector, X::DataFrameRow)
-    return first([1 X[:total_alive_bl]] * beta)
-    #return beta[1]
-end
+One approach is to \textit{de-bias} the plug-in estimate by estimating the first-order bias via empirical mean of the estimated EIF and subtracting it from the initial plug-in estimate:
+\begin{align}
+    \hat{\psi}^{\mathsf{onestep}} \equiv \psi(\hat{P}) - \frac{1}{n}\sum_{i=1}^n \D_{\hat{P}}(Z_i).
+\end{align}
+If conditions can be found to control the second-order remainder term $\R(\P, \hat{P})$, it is possible to show consistency and asymptotic normality. Such estimators are typically referred to as \textit{one-step estimators} because they are analogous to a a one-step Newton-Raphson update, with the EIF playing the role of the gradient of the functional \citep{fisher2021}. Under appropriate rate conditions on the nuisance estimators, $\hat{\psi}^{\mathsf{onestep}}$ is asymptotically normal and efficient. A drawback of this approach is that estimator is not guaranteed to lie in the parameter space; in our case, for example, a one-step estimate of the random replacement parameter may fall outside of $[0, 1]$. 
 
-function squared_error_loss(t, m)
-    return (t - m)^2
-end
+TMLE takes a different approach by carefully \textit{fluctuating} the initial estimate $\hat{P}$ of $\P$ along a low-dimensional parametric submodel in such a way that the updated estimate $\hat{P}^*$ (approximately) solves the empirical EIF estimating equation; that is,
+\begin{align}
+    \frac{1}{n}\sum_{i=1}^n \D_{\hat{P}^*}(Z_i) \approx 0.
+\end{align}
+A targeted estimate of $\psi_{a}^{\mathsf{replacement}}$ is then formed by plugging $\hat{P}^*$ into the parameter $\psi_{a}^{\mathsf{replacement}}(\cdot)$. As a substitution estimator, the targeted estimate is guaranteed by construction to lie in the parameter space. Constructing an appropriate fluctuation scheme that zeros the empirical first-order term is primarily a mathematical exercise; we give a complete description of the proposed algorithm in Appendix~\ref{appendix:tmle}. In terms of consistency, the form of the EIF implies that the resulting estimator will be consistent if \textit{either} the outcome regression $\mu$ or the attempt assignment model $\pi$ is estimated consistently, a result commonly referred to as \textit{double robustness}. 
 
-Y = data[:, :curr_use_inj_adj]
-A = data[:, :treatment]
-X = data[:, [:total_alive_bl]]
-    
-result = TargetedMSM.treatment_effect_modification(
-    Y,
-    A,
-    X,
-    nothing,
-    nothing,
-    2, 
-    squared_error_loss,
-    linear_working_model; 
-    Qbar = data[:, :Qbar],
-    Qbar0 = data[:, :Qbar0],
-    Qbar1 = data[:, :Qbar1],
-    g = data[:, :g],
-    linear = false,
-    bayes = true,
-    iterations = 50_000,
-    seed = 1234,
-    #proposal_sd = 0.09375,
-    include_prior = true,
-    prior = Normal(0, 1)
-);
+By design, the targeted estimate eliminates the leading (first-order) term in the von Mises expansion. The remaining task is to establish conditions under which the second-order remainder term $\R(\P, \hat{P}^*)$ is asymptotically negligible. In our setting, the form the remainder can be shown to have a product form, involving the estimation errors of the outcome regression $\mu$ and the attempt assignment model $\pi$. As a result, the targeted estimator can be shown to be asymptotically normal and efficient as long as $\hat{\mu}$ and $\hat{\pi}$ converge at rates faster than $n^{-1/4}$. This result, referred to as \textit{rate double-robustness}, is the crucial finding that allows the use of data-adaptive nuisance estimation methods that converge at slower than parametric rates.
 
-total_alive = [0:9;]
-pred = result[:beta][1] .+ result[:beta][2] * total_alive
+Our estimation strategy relies crucially on \textit{cross-fitting}. Cross-fitting proceeds by  splitting the data into multiple disjoint folds. For each fold, nuisance estimators are fit using observations in the complementary folds (the training set), and out-of-sample predictions are produced for observations in the held-out (validation) fold. Iterating over folds yields a complete set of nuisance predictions, where each prediction is computed out of sample. Cross-fitting allows us to use arbitrary, potentially complex nuisance estimators. Without cross-fitting, asymptotic normality results typically require restricting the complexity of the nuisance estimators. A classical way to achieve this is by assuming nuisance estimators fall in a Donsker class, verified through entropy arguments \citep{vanderVaart1996}. Cross-fitting obviates these requirements, and is now  a standard practice in semi-parametric estimation.
 
-h = scatter(data[:, :total_alive_bl], result[:Psi_star])
-h = scatter!(total_alive, pred)
-
-density(result[:beta_post][:, 1])
-plot!(Normal(result[:beta][1], result[:beta_se][1] / sqrt(size(data)[1])))
-
-density(result[:beta_post][:, 2])
-plot!(Normal(result[:beta][2], result[:beta_se][2] / sqrt(size(data)[1])))
-
-h = scatter(result[:epsilon_post][:, 1], result[:beta_post][:, 1])
-h = scatter(result[:epsilon_post][:, 2], result[:beta_post][:, 2])
-
-CSV.write(
-    "results/pnas_frequentist.csv",
-    DataFrame(
-        name  = ["beta1", "beta2"],
-        n     = repeat([size(data)[1]], 2),
-        beta  = result[:beta],
-        lower = result[:beta_lower][:, 1],
-        upper = result[:beta_upper][:, 1],
-        se    = result[:beta_se][:, 1],
-    )
-)
-
-CSV.write(
-    "results/pnas_posterior.csv",
-    DataFrame(
-        beta1 = result[:beta_post][:, 1],
-        beta2 = result[:beta_post][:, 2],
-        epsilon1 = result[:epsilon_post][:, 1],
-        epsilon2 = result[:epsilon_post][:, 2]
-    )
-)
+We are now  ready to formally state the statistical properties of the cross-fitted, targeted estimator of $\psi_{a}^{\mathsf{replacement}}$.
+\begin{theorem}[Consistency and asymptotic normality]
+    Let $\| \cdot \|$ denote the $L_2(\P)$ norm, and $\hat{\psi}_a^{\mathsf{replacement}}$ be the TMLE estimate of $\psi_{a}^{\mathsf{replacement}}$. Suppose that either $\| \hat{\pi} - \pi \| = o_{P}(1)$ or $\| \hat{\mu} - \mu \| = o_{P}(1)$. Then 
+    \begin{align}
+        \hat{\psi}_{a}^{\mathsf{replacement}} - \psi_{a}^{\mathsf{replacement}} = o_{P}(1) .
+    \end{align}
+    Suppose that both $\| \hat{\pi} - \pi \| = o_{P}(n^{-1/4})$ and $\| \hat{\mu} - \mu \| = o_{P}(n^{-1/4})$. Then 
+    \begin{align}
+        \sqrt{n}\left( \hat{\psi}_a^{\mathsf{replacement}} - \psi_{a}^{\mathsf{replacement}} \right) \leadsto N\left(0, \E_{P}\left[ \D^{\mathsf{replacement}}_{P}(Z)^2 \right]\right).
+    \end{align}
+\end{theorem}
--- example/data_preparation_pnas.R
+++ example/data_preparation_pnas.R
@@ -1,6 +1,4 @@
 library(tidyverse)
-library(tmle)
-library(superleaner)
 library(haven)
 
 covariates <- c("curr_use_inj_adj_bl",
@@ -14,13 +12,13 @@ covariates <- c("curr_use_inj_adj_bl",
   "religion_r_bl",
   "ethnicity_r_bl")
 
-dat <- read_dta("~/TargetedMSM/pnas_data/MFPS_PNAS_MAIN_ITT_DATA.dta") %>%
+dat <- read_dta("example/data/MFPS_PNAS_MAIN_ITT_DATA.dta") %>%
   mutate_at(vars(treatment, curr_use_inj_adj_bl, area_bl, age_group_bl, ever_use_bl, edu_primary_bl, work_bl, religion_r_bl, ethnicity_r_bl), as.factor) %>%
   filter(year == 2018) %>%
   select(curr_use_inj_adj, treatment, !!covariates) %>%
   drop_na()
 
-cairo_pdf("~/TargetedMSM/plots/pnas_effect_modifier.pdf", width = 9, height = 4)
+cairo_pdf("~plots/pnas_effect_modifier.pdf", width = 9, height = 4)
 count(dat, total_alive_bl) %>%
   ggplot(aes(x = factor(total_alive_bl), y = n)) +
   geom_bar(stat = "identity") +
@@ -31,63 +29,17 @@ count(dat, total_alive_bl) %>%
   cowplot::panel_border() +
   labs(x = "Number of children at baseline")
 dev.off()
- 
+
 mean(as.numeric(dat$treatment) - 1)
 
 # Replicate original analysis
 # Unadjusted
-lm(curr_use_inj_adj ~ 1 + treatment, data = filter(dat, year == 2018))
+lm(curr_use_inj_adj ~ 1 + treatment, data = dat)
 
 # Adjusted
 f <- as.formula(glue::glue("curr_use_inj_adj ~ 1 + treatment + {str_c(covariates, collapse = ' + ')}"))
 lm(f, data = dat)
 
-SL.library <- c(
-  "SL.glm", 
-  "SL.xgboost", 
-  "SL.glm.interaction",
-  "SL.rpart", 
-  "SL.mean",
-  "SL.ranger", 
-  "SL.glmnet",
-  "SL.hal9001"
-)
+write_csv(dat, "~example/data/pnas_data.csv")
 
-fit <- tmle(
-  Y = as.numeric(dat$curr_use_inj_adj),
-  A = as.numeric(dat$treatment) - 1,
-  W = as.data.frame(dat[, covariates]),
-  Q.SL.library = SL.library,
-  g.SL.library = SL.library,
-  family = "binomial",
-  verbose = TRUE
-)
 
-results <- tibble(
-  age = 13:28,
-  fit = map(age, function(age) lm(curr_use_inj_adj ~ 1 + treatment, data = filter(dat, sex_age_bl == age))),
-  coef = map_dbl(fit, function(fit) coef(fit)[[2]])
-)
-ggplot(results, aes(x = age, y = coef)) +
-  geom_point()
-
-results <- tibble(
-  total_alive = 1:6,
-  fit = map(total_alive, function(total_alive) lm(curr_use_inj_adj ~ 1 + treatment, data = filter(dat, total_alive_bl == total_alive))),
-  coef = map_dbl(fit, function(fit) coef(fit)[[2]])
-)
-ggplot(results, aes(x = total_alive, y = coef)) +
-  geom_point()
-
-plot(dat$sex_age_bl, fit$Qstar[, "Q1W"] - fit$Qstar[, "Q0W"])
-
-augmented_data <- dat %>%
-  mutate(Qbar0 = fit$Qinit$Q[, "Q0W"],
-         Qbar1 = fit$Qinit$Q[, "Q1W"],
-         Qbar  = ifelse(treatment == 1, Qbar1, Qbar0),
-         g     = fit$g$g1W,
-         treatment = as.numeric(treatment) - 1)
-
-write_csv(augmented_data, "~/TargetedMSM/pnas_data.csv")
-
-     
